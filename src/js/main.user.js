e5f19e52df0de3b750362d27d17626c1791bf9a1
admin <admin@example.com>
2021-10-23 02:20:29 +0000

    content update

--- src/js/main.user.js
+++ src/js/main.user.js
@@ -524,7 +524,7 @@ const MASTER_OF_FORUMS = () => {
   });
 
   // File host - Global
-  MONKEY_MENU.name = '\u{1F4A0} \u{1F4E4} \u{6587}\u{4EF6}\u{4E0A}\u{4F20} \u{279C} \u{5168}\u{7403}\u{8282}\u{70B9}';
+  MONKEY_MENU.name = '\u{1F4A0} \u{1F4E4} \u{6587}\u{4EF6}\u{4E0A}\u{4F20} \u{279C} \u{4E2D}\u{56FD}\u{8282}\u{70B9}';
   GM_registerMenuCommand(MONKEY_MENU.name, async () => {
     MAIN.tips.fileboard.style.display = 'none';
     try {
@@ -574,7 +574,7 @@ const MASTER_OF_FORUMS = () => {
   });
 
   // File host - Global (Command+V or Ctrl+V)
-  MONKEY_MENU.name = `\u{1F4A0} \u{1F4E4} \u{6587}\u{4EF6}\u{4E0A}\u{4F20} \u{279C} \u{5168}\u{7403}\u{8282}\u{70B9} \u{30FB} ${USER_AGENT.includes('Mac OS X') ? 'Command' : 'Ctrl'} + V`;
+  MONKEY_MENU.name = `\u{1F4A0} \u{1F4E4} \u{6587}\u{4EF6}\u{4E0A}\u{4F20} \u{279C} \u{4E2D}\u{56FD}\u{8282}\u{70B9} \u{30FB} ${USER_AGENT.includes('Mac OS X') ? 'Command' : 'Ctrl'} + V`;
   GM_registerMenuCommand(MONKEY_MENU.name, async () => {
     MAIN.can.pasteUpload = true;
     MAIN.fn.fileUploadFunction = MAIN.fn?.fileUploadToGlobal;
@@ -1020,7 +1020,7 @@ const MASTER_OF_FORUMS = () => {
     } else {
       GM_notification({
         title: '\u{8BBA}\u{575B}\u{5927}\u{5E08}',
-        text: '\u{6587}\u{4EF6}\u{4E0A}\u{4F20}（\u{5168}\u{7403}\u{8282}\u{70B9}）',
+        text: '\u{6587}\u{4EF6}\u{4E0A}\u{4F20}（\u{4E2D}\u{56FD}\u{8282}\u{70B9}）',
         image: 'https://cdn.jsdelivr.net/gh/master-of-forums/master-of-forums/public/images/bull.webp',
         timeout: 4 * 1000,
       });

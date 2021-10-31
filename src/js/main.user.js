b1a9b2b387078077669568fd4e8482ae63fe3ede
admin <admin@example.com>
2021-10-31 02:45:59 +0000

    content update

--- src/js/main.user.js
+++ src/js/main.user.js
@@ -1529,10 +1529,13 @@ const MASTER_OF_FORUMS = () => {
     if (MAIN.data?.hostname === 'ωωω_hostloc_com' && SEARCH_PARAMS.get('action') === 'newthread' && document.getElementById('subject')?.value) {
       const CHAR = new Map([
         ['\u{4E73}', 'Rǔ'],
-        ['\u{5907}\u{6848}', 'Bèiàn'],
         ['\u{59E6}', 'Jiān'],
         ['\u{5AD6}', 'Piáo'],
+        ['\u{5FAE}\u{535A}', 'WēiBó'],
         ['\u{6DEB}', 'Yín'],
+        ['\u{7FFB}\u{5899}', 'FānQiáng'],
+        ['GFW', '[G‧F‧W]'],
+        ['VPN', '[V‧P‧N]'],
       ]);
       let content = document.getElementById('subject')?.value?.trim();
       for (const [key, value] of CHAR) {

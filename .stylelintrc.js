3e4150348636796c31d3f2ff20fbfd3ad8a97669
admin <admin@example.com>
2021-11-04 05:49:59 +0000

    content update

--- .stylelintrc.js
+++ .stylelintrc.js
@@ -2,7 +2,10 @@ module.exports = {
   extends: ['stylelint-config-standard', 'stylelint-config-recess-order'],
   plugins: ['stylelint-order'],
   rules: {
+    'max-line-length': null,
     'no-descending-specificity': null,
+    'selector-class-pattern': null,
+    'selector-id-pattern': null,
     'selector-type-no-unknown': null,
     'value-keyword-case': null,
   },
